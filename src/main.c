1a727839572ba47547beb1bd6ceeb73f4d9462d4
Iurii Ovcharenko <iurii.ovcharenko@dev.zodiac.tv>
2014-12-04 14:45:21 +0200

    Force major

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
   int i;
 
-  printf("%s\n", PROG);
+  printf("%s for %d\n", PROG, FORCEMAJOR);
   for (i = 0; i < argc; i++)
     printf("%-3d : %s\n", i, argv[i]);
 
